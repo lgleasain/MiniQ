0e56930287b6dcf9453862282324dcf13822b2d8
Lance Gleason <lgleasain@yahoo.com>
2011-11-12 09:31:50 -0500

    updating survo and web page

--- events.rb
+++ events.rb
@@ -1,21 +1,6 @@
 require 'bayeux'
 
-events.asterisk.manager_interface.each do |event|
-  p 'a event was received'
-  p event.name
-  case event.name
-  when 'Newstate'
-    Bayeux.publish('/caller/join', { :id => event.headers['Channel'], 
-                                     :number => event.headers['CallerIDNum'],
-                                     :joinedAt => Time.now.to_i })
-  when 'Hangup'
-    Bayeux.publish('/caller/leave', { :id => event.headers['Channel'] })
-  end
-end
-
 events.asterisk.before_call.each do |call|
-  extension = call.variables[:extension]
-  ahn_log "New call with extension #{extension}"
 
   Bayeux.publish('/caller/join', { :id => call.variables[:channel], 
                                     :number => call.variables[:calleridname],
--- lib/bayeux.rb
+++ lib/bayeux.rb
@@ -8,6 +8,5 @@ module Bayeux
   def self.publish(channel, data)
     message = {'channel' => channel, 'data' => data.to_hash}
     Net::HTTP.post_form(ENDPOINT, :message => JSON.unparse(message))
-    p "publishing #{message}"
   end
 end
