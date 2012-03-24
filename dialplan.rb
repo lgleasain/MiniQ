bf791636a9e2fed2ef7939939a557b37fa69f341
Lance Gleason <lgleasain@yahoo.com>
2012-03-23 23:40:11 -0400

    modifying to recruiter 2012

--- dialplan.rb
+++ dialplan.rb
@@ -1,34 +1,60 @@
-adhearsion {
-  simon_game
+trolo {
+  play 'http://net1.madringtones.org/data/13/451546/files/451546.mp3'  
 }
 
 tropo_agi {
-  choice = input 1, :play => 'press 1 to wait or 2 to send a text, or 3 to dial another person'
-  if choice == '1'
-    play 'http://net1.madringtones.org/data/13/451546/files/451546.mp3'
-  elsif choice == '2'
-    tel_number = input 10, :play => 'enter the number you want us to text'  
-    play 'you entered'
-    play tel_number
-    message_choice = input 1, :play => 'press 1 to send test results,  2 for nigerian bank scam or 3 for google password reset scare'
-    message = ''
-    if message_choice == '1'
-	message = 'your test results are in,  please call (866) 676-5817 to discuss with nurse.'
-    elsif message_choice == '2'
-	message = 'please email friend@nigriabank.com to discuss tansfer of 1 billion dollars to your band account'
-    else
-	message = 'your gmail account password has been reset.  If this was not you go to www.gggmail.com to reset'
-    end
-    
-    oneapi = Smsified::OneAPI.new(:username => 'lgleasain', :password => '')
-    oneapi.send_sms :address => "1#{tel_number}", :message => message, :sender_address => '14045667572'
-  else
-    num_to_call = input 10, :play => 'please enter number to call' 
-    play num_to_call
-    num_from = input 10, :play => 'please enter the number that you are calling from'
-    play num_from
+  menu = "Welcome to the recruiter 2012.  " +
+          "Press 1 to hear a rick roll. " +
+          "Press 2 to robocall a friend with this menu. " +
+          "Press 3 to send a snarky text to a recruiter. " +
+          "Press 4 to rick roll someone else."
 
-    token = ''
-    Net::HTTP.post_form(URI.parse('http://api.tropo.com/1.0/sessions'), "token" => token, "destination" => "1#{num_to_call}", "tropo_tag" => 666, "caller_id" => num_from, "caller_id_name" => 'beavis')
+  choice = input 1, :play => menu
+
+  case choice
+    when '1'
+      dial('+14048574684')
+
+    when '2'
+      num_to_call = input 10, :play => 'please enter number to call' 
+      play num_to_call
+      num_from = input 10, :play => 'please enter the number that you want to call from'
+      play num_from
+      token = ''
+      Net::HTTP.post_form(URI.parse('http://api.tropo.com/1.0/sessions'), "token" => token, "destination" => "1#{num_to_call}", "tropo_tag" => 666, "caller_id" => num_from, "caller_id_name" => 'beavis')
+
+    when '3'
+      tel_number = input 10, :play => 'enter the number you want us to text'  
+      play 'you entered'
+      play tel_number
+      message_choice = input 1, 
+        :play => 'press 1 to say no thank you. ' + 
+               ' 2 to tell the recruiter to buzz off. ' + 
+               'or 3 to refer to another source a great candidates.'
+      message = ''
+
+      case message_choice
+        when '1'
+          message = 'I am not in the job market,  ' + 
+                    'but when I want to feel like a used car I will call you.'
+        when '2'
+          message = 'You guys are worse than telemarketers,  please put me on your do '  + 
+                    'not call list' 
+        else
+	  message = 'I am not in the market right now but the people here have some ' +
+                    'wonderfull candidates  (386) 462-7779.'
+      end
+
+      oneapi = Smsified::OneAPI.new(:username => 'lgleasain', :password => '')
+      oneapi.send_sms :address => "1#{tel_number}", :message => message, :sender_address => '14045667572'
+
+    else
+      num_to_call = input 10, :play => 'please enter number to call' 
+      play num_to_call
+      num_from = input 10, :play => 'please enter the number that you are calling from'
+      play num_from
+
+      token = ''
+      Net::HTTP.post_form(URI.parse('http://api.tropo.com/1.0/sessions'), "token" => token, "destination" => "1#{num_to_call}", "tropo_tag" => 666, "caller_id" => num_from, "caller_id_name" => 'beavis')
   end
 }
